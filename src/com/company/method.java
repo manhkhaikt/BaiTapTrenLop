87a1a090bdb17a46ca473de040e037d03bcc1959
manhkhaikt <manhkhai98@gmail.com>
2018-10-22 20:25:10 +0700

    method

--- src/com/company/method.java
+++ src/com/company/method.java
@@ -19,8 +19,8 @@ public class method {
         System.out.println("So doi dien: " + circleOfNumbers(n, firstNumber));
         System.out.println("-------------------------------------------");
 
-        int[][] matrix={{0,1,1,2},{0,5,0,0},{2,0,3,3}};
-        System.out.println("CodeBots: "+matrixElementsSum(matrix));
+        int[][] matrix = {{0, 1, 0, 2}, {0, 5, 0, 0}, {2, 0, 3, 3}};
+        System.out.println("CodeBots: " + matrixElementsSum(matrix));
     }
 
     static boolean checkPalindrome(String a) {
@@ -29,13 +29,15 @@ public class method {
         if (a.length() % 2 != 0) {
             int count1 = 1;
             for (int i = a.length() / 2; i < a.length() - 1; i++) {
-                if (newArr[a.length() / 2 + count1] != newArr[a.length() / 2 - count1]) test = 1;
+                if (newArr[a.length() / 2 + count1] != newArr[a.length() / 2 - count1])
+                    test = 1;
                 count1++;
             }
         } else if (a.length() % 2 == 0) {
             int count2 = 1;
             for (int j = a.length() / 2; j < a.length(); j++) {
-                if (newArr[j] != newArr[a.length() / 2 - count2]) test = 1;
+                if (newArr[j] != newArr[a.length() / 2 - count2])
+                    test = 1;
                 count2++;
             }
 
@@ -47,9 +49,9 @@ public class method {
     static int circleOfNumbers(int n, int firstNumber) {
         int numOpposite = 0;
         for (int i = 0; i < n; i++) {
-            if (firstNumber > n / 2) {
+            if (firstNumber > n / 2 && firstNumber < n) {
                 numOpposite = firstNumber - n / 2;
-            } else if (firstNumber < n / 2) {
+            } else if (firstNumber < n / 2 && firstNumber > 0) {
                 numOpposite = firstNumber + n / 2;
             }
         }
@@ -60,8 +62,10 @@ public class method {
         int sum = 0;
         for (int j = 0; j < matrix[0].length; j++) {
             for (int i = 0; i < matrix.length; i++) {
-                if (matrix[i][j] == 0) break;
-                else sum += matrix[i][j];
+                if (matrix[i][j] == 0)
+                    break;
+                else
+                    sum += matrix[i][j];
             }
         }
 
